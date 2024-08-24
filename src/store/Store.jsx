a8f841542f33ee3e3506381463417ed70261255f
saurabh-159 <jshaurya680@gmail.com>
2024-08-24 15:44:33 +0530

    some error

--- src/store/Store.jsx
+++ src/store/Store.jsx
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { movieSlice } from './reducers/MovieSlice'
 import movieReducer from "./reducers/MovieSlice";
-import tvReducer from "./reducers/TvSlice";
+import tvReducer from "./reducers/tvSlice";
 import peopleReducer from  "./reducers/PeopleSlice"
 
 export const store = configureStore({
